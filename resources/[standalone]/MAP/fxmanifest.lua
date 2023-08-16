b67bb102f149a29ad18509b983fc812837e44c6c
Swat <readyforswat@gmail.com>
2023-08-16 14:55:10 -0700

    Alot of random

--- resources/[standalone]/MAP/fxmanifest.lua
+++
@@ -1,5 +0,0 @@
-fx_version "cerulean"
-game "gta5"
-
-author "Swat"
-description "Map"
---
+++ resources/[vehicles]/[helicopters]/[us]/Blackhawk/fxmanifest.lua
@@ -0,0 +1,24 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+fx_version 'cerulean'
+game 'gta5'
+
+description 'US Helicopters'
+Author "Swat"
+
+files {
+    'carcols.meta',
+    'carvariations.meta',
+    'dlctext.meta',
+    'handling.meta',
+    'vehicles.meta',
+}
+
+data_file 'VEHICLE_METADATA_FILE' 'carcols.meta' 
+data_file 'HANDLING_FILE' 'carvarations.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'dlctext.meta'
+data_file 'WEAPONINFO_FILE' 'handling.meta'
+data_file 'WEAPON_METADATA_FILE' 'vehicle.meta'
+
+
+
+ 
